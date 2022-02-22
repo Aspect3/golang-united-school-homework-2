f4dd6180c5524d383cebe97ab7d03c3ac70b5732
Aspect3 <aspect3@mail.ru>
2022-02-22 20:57:39 +0500

    test

---
+++ _solution.go
@@ -0,0 +1,26 @@
+package square
+
+import (
+	"math"
+)
+
+type CustomVar int
+
+const (
+	SidesCircle   = 0
+	SidesTriangle = 3
+	SidesSquare   = 4
+)
+
+func CalcSquare(sideLen float64, sidesNum CustomVar) float64 {
+	switch sidesNum {
+	case SidesCircle:
+		return math.Pi * math.Pow(sideLen, 2)
+	case SidesTriangle:
+		return math.Sqrt(3) / 4 * math.Pow(sideLen, 2)
+	case SidesSquare:
+		return math.Pow(sideLen, 2)
+	default:
+		return 0
+	}
+}
--- solution.go
+++ solution.go
@@ -4,22 +4,12 @@ import (
 	"math"
 )
 
-type CustomVar int
-
-const (
-	SidesCircle   = 0
-	SidesTriangle = 3
-	SidesSquare   = 4
-)
-
-func CalcSquare(sideLen float64, sidesNum CustomVar) float64 {
+func CalcSquare(sideLen float64, sidesNum int) float64 {
 	switch sidesNum {
-	case SidesCircle:
+	case 0:
 		return math.Pi * math.Pow(sideLen, 2)
-	case SidesTriangle:
-		return math.Sqrt(3) / 4 * math.Pow(sideLen, 2)
-	case SidesSquare:
-		return math.Pow(sideLen, 2)
+	case >0:
+		return (sidesNum * math.Pow(sideLen, 2))/(4*math.Tan(360/2*sidesNum))
 	default:
 		return 0
 	}
